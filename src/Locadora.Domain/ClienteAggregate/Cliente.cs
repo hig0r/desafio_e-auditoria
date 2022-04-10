0b56f85ac21460bc4b9702a18f514406ad6cfa60
higor <github@higor.ninja>
2022-04-10 19:02:42 -0300

    entidades validar valores nos setters

--- src/Locadora.Domain/ClienteAggregate/Cliente.cs
+++ src/Locadora.Domain/ClienteAggregate/Cliente.cs
@@ -6,6 +6,9 @@ namespace Locadora.Domain.ClienteAggregate;
 public class Cliente : Entity
 {
     private List<Locacao> _locacoes = new();
+    private string _nome = null!;
+    private DateTime _dataNascimento;
+    private Cpf _cpf = null!;
 
     private Cliente()
     {
@@ -13,18 +16,43 @@ public class Cliente : Entity
 
     public Cliente(string nome, Cpf cpf, DateTime dataNascimento)
     {
-        ArgumentNullException.ThrowIfNull(nome);
-        ArgumentNullException.ThrowIfNull(cpf);
-        if (dataNascimento.Date > DateTime.Today)
-            throw new ArgumentException("Data de nascimento não pode ser maior que hoje");
         Nome = nome;
         Cpf = cpf;
-        DataNascimento = dataNascimento.Date;
+        DataNascimento = dataNascimento;
     }
-    
-    public string Nome { get; private set; } = null!;
-    public Cpf Cpf { get; private set; } = null!;
-    public DateTime DataNascimento { get; private set; }
+
+    public string Nome
+    {
+        get => _nome;
+        set
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            _nome = value;
+        }
+    }
+
+    public Cpf Cpf
+    {
+        get => _cpf;
+        private init
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            _cpf = value;
+        }
+    }
+
+    public DateTime DataNascimento
+    {
+        get => _dataNascimento;
+        set
+        {
+            if (value == default)
+                throw new ArgumentException("Especifique uma data.");
+            if (value.Date > DateTime.Today)
+                throw new ArgumentException("Data de nascimento não pode ser maior que hoje");
+            _dataNascimento = value.Date;
+        }
+    }
 
     public ICollection<Locacao> Locacoes => _locacoes.AsReadOnly();
 }
--- src/Locadora.Domain/FilmeAggregate/Filme.cs
+++ src/Locadora.Domain/FilmeAggregate/Filme.cs
@@ -7,24 +7,31 @@ namespace Locadora.Domain.FilmeAggregate;
 public class Filme : Entity
 {
     private List<Locacao> _locacoes = new();
+    private string _titulo;
+    private int _classificacaoIndicativa;
 
-    private Filme()
+    public string Titulo
     {
+        get => _titulo;
+        set
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            _titulo = value;
+        }
     }
 
-    public Filme(string titulo, int classificacaoIndicativa, bool lancamento)
+    public int ClassificacaoIndicativa
     {
-        ArgumentNullException.ThrowIfNull(titulo);
-        if (classificacaoIndicativa is not 0 or 10 or 12 or 14 or 16 or 18)
-            throw new ArgumentException("Classificação indicativa deve ser 0 (L), 10, 12, 14, 16 ou 18");
-        Titulo = titulo;
-        ClassificacaoIndicativa = classificacaoIndicativa;
-        Lancamento = lancamento;
+        get => _classificacaoIndicativa;
+        set
+        {
+            if (value is not 0 or 10 or 12 or 14 or 16 or 18)
+                throw new ArgumentException("Classificação indicativa deve ser 0 (L), 10, 12, 14, 16 ou 18");
+            _classificacaoIndicativa = value;
+        }
     }
 
-    public string Titulo { get; private set; }
-    public int ClassificacaoIndicativa { get; private set; }
-    public bool Lancamento { get; private set; }
+    public bool Lancamento { get; set; }
 
     public IReadOnlyCollection<Locacao> Locacoes => _locacoes.AsReadOnly();
 
